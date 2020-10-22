a95da86fcd893ff62e3d8c7c8fea6dd0aa267490
glorious-communicator-0033 <clinton.weaver@gmail.com>
2020-10-22 01:20:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ function nowServing(line) {
 
 function currentLine(line) {
   var currently = "The line is currently: "
+  if(line.length=0) {
+    return "The line is currently empty"
+  }
   for(let i = 0; i < line.length; line++) {
     currently += `${i}. ${line[i]}, `
   }
