1e346d7dc498445371c59fc5506232c8e1a41602
glorious-communicator-0033 <clinton.weaver@gmail.com>
2020-10-22 01:13:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function takeANumber(line, name) {
   line.push(name)
   return `Welcome, ${name}. You are number ${(line.length)} in line.`
 }
+
+function nowServing(line) {
+  if(line.length > 0) {
+    name=line[0]
+    line.splice(1)
+    return `Now serving ${name}`
+  }
+}
